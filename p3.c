e7268ae4cd81f431952302ddb89fd432afd59853
siddique2003 <56973909+siddique2003@users.noreply.github.com>
2023-09-09 18:41:07 +0500

    Add files via upload

---
+++ p3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+
+int main(int argc, char *argv[]) {
+    printf("hello world (pid:%d)\n", (int) getpid());
+    int rc = fork();
+    
+    if (rc < 0) {
+        // fork failed; exit
+        fprintf(stderr, "fork failed\n");
+        exit(1);
+    } else if (rc == 0) {
+        // child (new process)
+        printf("hello, I am child (pid:%d)\n", (int) getpid());
+        char *browser = "chromium-browser";  // Use the correct executable name
+        char *url = "https://www.example.com";  // Change to your desired URL
+        
+        // Launch Chromium with the given URL
+        execlp(browser, browser, url, NULL);
+        
+        // If execlp fails, an error message will be printed
+        perror("exec failed");
+        exit(1);
+    } else {
+        // parent goes down this path (original process)
+        int wc = wait(NULL);
+        printf("hello, I am parent of %d (wc:%d) (pid:%d)\n", rc, wc, (int) getpid());
+    }
+    return 0;
+}
